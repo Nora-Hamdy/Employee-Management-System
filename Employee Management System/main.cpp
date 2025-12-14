951d2460bac8de1e75ea1968458159bbf4d2bd46
Nora-Hamdy <NoraHamdyAly@gmail.com>
2025-12-14 10:30:15 +0200

    feat(main): implement main.cpp to initialize services and run EmployeeUI.

--- Employee Management System/main.cpp
+++ Employee Management System/main.cpp
@@ -1,4 +1,32 @@
+#include "include/ui/EmployeeUI.h"
+#include "include/helpers/ConfigManager.h"
+#include "include/helpers/consoleHelper.h"
+#include "include/repository/EmployeeRepository.h"
+#include "include/services/EmployeeService.h"
+#include<iostream>
+#include<memory>
+using namespace std;
+
 int main()
 {
+	auto configManager = ConfigManager::instance();
+	configManager.load();
+	auto fileResult = configManager.get("employee_file");
+	if (fileResult.status == OperationStatus::Success)
+	{
+		unique_ptr<IEmployeeRepository> employeeRepository = make_unique<EmployeeRepository>(fileResult.data.value());
+		shared_ptr<IEmployeeService> employeeService = make_shared<EmployeeService>(move(employeeRepository));
+		auto employeeServiceResult = employeeService->initialize();
+		if (employeeServiceResult.status == OperationStatus::Success)
+		{
+			EmployeeUI EmployeeUI(move(employeeService));
+			EmployeeUI.run();
+		}
+		else
+			printError(employeeServiceResult.message);
+	}
+	else
+		printError(fileResult.message);
+	system("pause");
 	return 0;
 }
